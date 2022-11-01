4c5b8437401644cbe1c90b7e1b5364141428f61a
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-01 10:20:16 +0000

    Code

---
+++ C++/28tech/mang/minchuaxuathien.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a,a+n);
+    int i = 0;
+    while(i<=n)
+    {
+        if (a[i] != a[i]+1 or a[i] == a[i+1])
+        {
+            if (a[i] != a[i+1]-1 && a[i] != a[i+1])
+            {
+                cout << a[i]+1 << " "; break;
+            }
+        i++;
+        }
+    }
+    return 0;
+}
