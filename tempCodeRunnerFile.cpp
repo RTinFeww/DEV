2e5c58ba0dc2a43b90f3fde331e3a8e07ac5312a
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-03 08:11:38 +0000

    Code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,25 +1,8 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-int main() {
-    float a, b, c, delta, x1, x2;
-    cin >> a >> b >> c;
-    delta = pow(b,2) - 4*a*c;
-    if (a != 0)
-    {
-        if (delta > 0)
-        {
-            x1 = (-b + sqrt(delta))/(2*a);
-            x2 = (-b - sqrt(delta))/(2*a);
-            cout << "x1 = " << x1 << endl << "x2 = " << x2;
-        }
-        else if (delta == 0)
-        {
-            cout << "x1 = x2 = " << -b/(2*a);
-        }
-        else cout << "Phương trình vô nghiệm";
-    }
-    else cout << "Phương trình không phải phương trình bậc hai";
+int main(int argc, char const *argv[])
+{
+    
     return 0;
 }
