5cc447d45ce8570fc1325fa29a63b601ab488617
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-19 06:52:30 +0000

    Code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <algorithm>
-#include <chrono>
+#include <ctime>
 #include <cstring>
 #include <cmath>
 #include <vector>
 #include <climits>
 #include <map>
+#define MAX_SIZE 1e7
+#define MIN_SIZE -1e7
 #define SIZE_OF_STRING(s) (s.size())
-#define SIZE_OF_ARRAY(a) (sizeof(a)/sizeof(a[0]))
+#define SIZE_OF_ARRAY(a)  (sizeof(a)/sizeof(a[0]))
 #define RTinFew
+using db = double;
 using ll = long long;
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    auto start = chrono::steady_clock::now();
     #ifndef RTinFew
-    freopen("nhap.in","r",stdin);
-    freopen("xuat.out","w",stdout);
+    freopen("D:\\DEV\\nhap.in","r",stdin);
+    freopen("D:\\DEV\\xuat.out","w",stdout);
     #endif
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout << "hi";
-    auto end = chrono::steady_clock::now();
-    cout << endl << "Execution time: " << chrono::duration<double>(end-start).count() << "s" << endl;
+    clock_t start, end;
+    start = clock();
+    //code
+    end = clock();
+    cout << "Execution time: " << double(end - start)/double(CLOCKS_PER_SEC) << "s";
     return 0;
 }
