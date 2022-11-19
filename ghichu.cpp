cf8c2b34f5aedc871c53f8c21487f36a97d2a987
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-19 04:08:27 +0000

    Code

---
+++ ghichu.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <map>
+#define MAX_SIZE 1e7
+#define MIN_SIZE -1e7
+#define SIZE_OF_STRING(s) (s.size())
+#define SIZE_OF_ARRAY(a)  (sizeof(a)/sizeof(a[0]))
+#define RTinFew
+using db = double;
+using ll = long long;
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    #ifndef RTinFew
+    freopen("D:\\DEV\\nhap.in","r",stdin);
+    freopen("D:\\DEV\\xuat.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    auto start = chrono::steady_clock::now();
+    // code
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cout << "Time used: " << chrono::duration<double>(diff).count() << "s" << endl;
+    return 0;
+}
+    
