25c588a099ceaa5c00114440ecc3a003ae5a83b0
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-19 06:30:49 +0000

    Code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
 #include <algorithm>
 #include <chrono>
-#include <ctime>
 #include <cstring>
 #include <cmath>
 #include <vector>
+#include <climits>
 #include <map>
-#define MAX_SIZE 1e7
-#define MIN_SIZE -1e7
 #define SIZE_OF_STRING(s) (s.size())
-#define SIZE_OF_ARRAY(a)  (sizeof(a)/sizeof(a[0]))
+#define SIZE_OF_ARRAY(a) (sizeof(a)/sizeof(a[0]))
 #define RTinFew
-using db = double;
 using ll = long long;
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    auto start = chrono::steady_clock::now();
     #ifndef RTinFew
-    freopen("D:\\DEV\\nhap.in","r",stdin);
-    freopen("D:\\DEV\\xuat.out","w",stdout);
+    freopen("nhap.in","r",stdin);
+    freopen("xuat.out","w",stdout);
     #endif
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    clock_t starts, ends;
-    starts = clock();
-    auto start = chrono::steady_clock::now();
-    for (int i = 0; i < 10000; cout << i++ << endl)
-    {
-        /* code */
-    }
-    ends = clock();
-    double time_taken = double(ends - starts)/double(CLOCKS_PER_SEC);
-    cout << "Time used: " << time_taken << "s";
+    cout << "hi";
     auto end = chrono::steady_clock::now();
-    auto diff = end - start;
-    cout << chrono::duration<double>(diff).count() << " s" << endl;
+    cout << endl << "Execution time: " << chrono::duration<double>(end-start).count() << "s" << endl;
     return 0;
 }
