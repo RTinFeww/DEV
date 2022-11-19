43dd0f84f48a1a9a36a721725ae746750cb20a7a
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-19 04:29:16 +0000

    Code

--- ghichu.cpp
+++ ghichu.cpp
@@ -4,13 +4,11 @@
 #include <cstring>
 #include <cmath>
 #include <vector>
+#include <climits>
 #include <map>
-#define MAX_SIZE 1e7
-#define MIN_SIZE -1e7
 #define SIZE_OF_STRING(s) (s.size())
-#define SIZE_OF_ARRAY(a)  (sizeof(a)/sizeof(a[0]))
+#define SIZE_OF_ARRAY(a) (sizeof(a)/sizeof(a[0]))
 #define RTinFew
-using db = double;
 using ll = long long;
 using namespace std;
 
@@ -23,7 +21,7 @@ int main(int argc, char const *argv[])
     #endif
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-        /* code */
+    //code
     auto end = chrono::steady_clock::now();
     cout << endl << "Execution time: " << chrono::duration<double>(end-start).count() << "s" << endl;
     return 0;
