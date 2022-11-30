a8622fdd8bb9dcb119c0ba9a1b3689bd876ac58e
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-11-30 05:33:27 +0000

    Code

---
+++ C++/LiThuyetSo/checksonguyento.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checknt(int n){
+    if (n < 2) return false;
+    for (int i = 2; i <= sqrt(n); ++i)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    for (int i = 2; i <= n; ++i)
+    {
+        if (checknt(i)) cout << i << " ";
+    }
+    return 0;
+}
