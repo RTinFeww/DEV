272526bb5605b1c48567c51f277693d2c8121405
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2022-12-04 12:34:54 +0000

    Code

--- C++/Dynamic_Programming/thamlam.cpp
+++ C++/Dynamic_Programming/thamlam.cpp
@@ -5,26 +5,6 @@ using namespace std;
 int main()
 {
     #ifndef RTinFew
-    freopen("nhap.in", "r", stdin);
-    freopen("xuat.out", "w", stdout);
-    #endif
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-	auto start = chrono::high_resolution_clock::now();
-	//code
-	auto end = chrono::high_resolution_clock::now();
-	double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count()*1e-9;
-	cout << "Time taken by program is : " << time_taken << " sec" << endl;
-	return 0;
-}
-//git add . && git commit -a -m "Code" && git push
-#include <bits/stdc++.h>
-#define RTinFew
-using namespace std;
-
-int main()
-{
-    #ifndef RTinFew
     freopen("D:\\DEV\\nhap.in", "r", stdin);
     freopen("D:\\DEV\\xuat.out", "w", stdout);
     #endif
