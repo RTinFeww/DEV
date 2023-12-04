36dcfc4c48b5e4e71256cf015b69873e54aba517
RTinFew <87790616+Rew-RTinFew@users.noreply.github.com>
2023-12-04 10:20:55 +0000

    Code

---
+++ Dart/Chemistry/chemical_symbol.dart
@@ -0,0 +1,17 @@
+class chemicalsymbol {
+    final String symbol;
+    final String nomenclature;
+    final int chemicalelementnumber;
+    final double atomicblock;
+    final String color;
+    final String state;
+    final String classify;
+    final Sring electron;
+    chemicalsymbol(this.symbol, this.nomenclature, this.chemicalelementnumber, this.atomicblock, this.color, this.state, this.classify, this.electron);
+}
+
+List <chemicalsymbol> libary = [
+    chemicalsymbol(
+        'H', 'Hydrogen', 1, 1.00811, 'Không màu', 'Chất khí', 'Phi kim', '1s^1'),
+    )
+]
